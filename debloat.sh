1d687f5a7ebb0b28620180c922cac10666ac812c
Samuel Tulach <samtulach@gmail.com>
2020-11-18 15:37:34 +0100

    fix gplay

--- debloat.sh
+++ debloat.sh
@@ -5,7 +5,7 @@ adb shell pm uninstall --user 0 com.android.chrome
 adb shell pm uninstall --user 0 com.google.android.apps.docs # google drive
 adb shell pm uninstall --user 0 com.google.android.apps.tachyon # google duo
 adb shell pm uninstall --user 0 com.google.android.gm # gmail
-adb shell pm uninstall --user 0 com.google.android.gms # gmail
+#adb shell pm uninstall --user 0 com.google.android.gms # google play services / breaks gplay
 adb shell pm uninstall --user 0 com.google.android.apps.photos
 adb shell pm uninstall --user 0 com.google.android.apps.wellbeing
 adb shell pm uninstall --user 0 com.google.android.calendar
@@ -21,10 +21,13 @@ adb shell pm uninstall --user 0 com.android.hotwordenrollment.xgoogle
 adb shell pm uninstall --user 0 com.google.android.apps.podcasts
 adb shell pm uninstall --user 0 com.google.android.apps.subscriptions.red
 adb shell pm uninstall --user 0 com.google.android.apps.magazines
+adb shell pm uninstall --user 0 com.google.android.documentsui
 
 adb shell pm uninstall --user 0 com.facebook.services
 adb shell pm uninstall --user 0 com.facebook.system
 adb shell pm uninstall --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.instagram.android
+adb shell pm uninstall --user 0 com.facebook.katana
 adb shell pm uninstall --user 0 com.heytap.cloud
 adb shell pm uninstall --user 0 com.heytap.mcs
 adb shell pm uninstall --user 0 com.heytap.openid
