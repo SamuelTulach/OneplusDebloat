01c916488c07cd657bc549764f345f5710302772
Samuel Tulach <samtulach@gmail.com>
2020-11-18 15:23:14 +0100

    first

---
+++ debloat.sh
@@ -0,0 +1,32 @@
+# adb shell pm list packages -s
+
+adb shell pm uninstall --user 0 com.google.android.apps.googleassistant
+adb shell pm uninstall --user 0 com.android.chrome
+adb shell pm uninstall --user 0 com.google.android.apps.docs # google drive
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon # google duo
+adb shell pm uninstall --user 0 com.google.android.gm # gmail
+adb shell pm uninstall --user 0 com.google.android.gms # gmail
+adb shell pm uninstall --user 0 com.google.android.apps.photos
+adb shell pm uninstall --user 0 com.google.android.apps.wellbeing
+adb shell pm uninstall --user 0 com.google.android.calendar
+adb shell pm uninstall --user 0 com.google.android.gms.location.history
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall --user 0 com.google.android.music
+adb shell pm uninstall --user 0 com.google.android.videos
+adb shell pm uninstall --user 0 com.google.android.youtube
+adb shell pm uninstall --user 0 com.google.android.apps.youtube.music
+adb shell pm uninstall --user 0 com.android.hotwordenrollment.okgoogle
+adb shell pm uninstall --user 0 com.android.hotwordenrollment.xgoogle
+adb shell pm uninstall --user 0 com.google.android.apps.podcasts
+adb shell pm uninstall --user 0 com.google.android.apps.subscriptions.red
+adb shell pm uninstall --user 0 com.google.android.apps.magazines
+
+adb shell pm uninstall --user 0 com.facebook.services
+adb shell pm uninstall --user 0 com.facebook.system
+adb shell pm uninstall --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.heytap.cloud
+adb shell pm uninstall --user 0 com.heytap.mcs
+adb shell pm uninstall --user 0 com.heytap.openid
+adb shell pm uninstall --user 0 com.netflix.mediaclient
+adb shell pm uninstall --user 0 com.netflix.partner.activation
